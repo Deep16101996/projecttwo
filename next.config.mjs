5e246c8b3e877f109113ef4008af3a924d0d05fc
Deep16101996 <deepakchougule2938@gmail.com>
2025-07-31 18:54:19 +0530

    second commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+     experimental: {
+    appDir: true,
+    esmExternals: 'loose',
+    serverComponentsExternalPackages: ['mongoose']
+     },
+      webpack(config) {
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    return config;
+  }
+};
 
 export default nextConfig;
