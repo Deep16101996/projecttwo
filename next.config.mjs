09f3892799ae37f0a20f2c3de1314dfd1780a6f0
Deep16101996 <deepakchougule2938@gmail.com>
2025-07-31 19:10:05 +0530

    asdf

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-     experimental: {
-    appDir: true,
-    esmExternals: 'loose',
-    serverComponentsExternalPackages: ['mongoose']
-     },
-      webpack(config) {
-    config.experiments = { ...config.experiments, topLevelAwait: true };
-    return config;
-  }
+    
+    
 };
 
 export default nextConfig;
